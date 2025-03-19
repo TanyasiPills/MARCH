45dc2a649c5c1957e5666c0385973b2d702f4863
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2025-03-19 18:10:09 +0100

    Parser functions

--- src/headers/Parser.h
+++ src/headers/Parser.h
@@ -1,7 +1,14 @@
 #include "AST.h"
 #include <vector>
+#include <queue>
 
 class Parser{
 private:
-    static std::vector<Token> tokens;
+    static std::queue<Token> tokens;
+    static std::shared_ptr<AST> Expression();
+    static std::shared_ptr<AST> Term();
+    static std::shared_ptr<AST> Factor();
+
+    static void ConsumeToken();
+
 };
